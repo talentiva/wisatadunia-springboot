5ed447f781d9c85d4d334b87cbfeba73bb0fedba
Hendy Irawan <hendy@hendyirawan.com>
2019-08-18 11:54:59 +0700

    Initial create project from https://start.spring.io

---
+++ src/main/java/com/hendyirawan/wisatadunia/WisataduniaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.hendyirawan.wisatadunia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisataduniaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisataduniaSpringbootApplication.class, args);
+	}
+
+}

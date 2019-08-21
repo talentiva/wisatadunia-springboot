249f89112faf347b23ff5ceacf84403503c1162f
Hendy Irawan <hendy@hendyirawan.com>
2019-08-21 10:58:00 +0700

    MvcConfig and RestConfig

--- src/main/java/com/hendyirawan/wisatadunia/MvcConfig.java
+++ src/main/java/com/hendyirawan/wisatadunia/MvcConfig.java
@@ -1,10 +1,10 @@
 package com.hendyirawan.wisatadunia;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Component
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/com/hendyirawan/wisatadunia/RestConfig.java
+++ src/main/java/com/hendyirawan/wisatadunia/RestConfig.java
@@ -1,10 +1,10 @@
 package com.hendyirawan.wisatadunia;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class RestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
